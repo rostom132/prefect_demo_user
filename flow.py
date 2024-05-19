c38516a81c25ca7800633afb1237715d41180ba8
Tien Tran <tien.trandinh99@gmai.com>
2024-05-19 13:58:02 +0700

    test

--- flow.py
+++ flow.py
@@ -1,35 +1,36 @@
 import json
 from prefect import flow, task
 from model.user_modal_pre import UserModelPre
+from model.user_raw_model import UserRaw
 
 @task(retries=2)
 def validate_input(input: str):
     print('PROCESS - validate user: ', input)
-    userJson = json.loads(input)
-    userData = UserModelPre(**userJson)
-    return userData
+    list_correct_data = []
+    list_failed_data = []
+
+    allUsersData = json.loads(input)
+    for userJson in allUsersData:
+        try:
+            userData = UserModelPre(**userJson)
+            list_correct_data.append(userData)
+        except Exception as e:
+            print('FAILED to parse: ', userData, e)
+            userData = UserRaw(**userData)
+            list_failed_data.append(userData)
+    return list_correct_data, list_failed_data
 
 @task(retries=3, retry_delay_seconds=3)
-def send_to_db(userData: UserModelPre):
-    print('SUCCESS - save to db', userData)
+def send_to_db(listUserData: UserModelPre):
+    print('SUCCESS - save to db', listUserData)
 
 @task(retries=3, retry_delay_seconds=3)
-def send_to_fail_db(userJsonData: str, exception):
-    print('FAILED - User raw json data: ', userJsonData)
-    print('FAILED - Exception: ', exception)
+def send_to_fail_db(listUserData: str):
+    print('FAILED - User raw json data: ', listUserData)
 
 
 @flow(log_prints=True)
-def user_pipeline(userJsonData: str = "{}"):
-    try:
-        userData = validate_input(userJsonData)
-    except Exception as e:
-        send_to_fail_db(userJsonData, e)
-    else:
-        send_to_db(userData)
-
-if __name__ == "__main__":
-    user_pipeline.deploy(
-        name="user-pipeline-image-deployment", 
-        work_pool_name="intern_demo"
-    )
+def user_pipeline(userJsonDatas: str = "[]"):
+    list_correct_data,  list_failed_data = validate_input(userJsonDatas)
+    send_to_db(list_correct_data)
+    send_to_fail_db(list_failed_data)
--- utils/repairer_after.py
+++ utils/repairer_after.py
@@ -5,6 +5,7 @@ import pickle
 with open('./utils/gender_predictor.pkl', 'rb') as f:
     gender_predictor = pickle.load(f)
 
+@task
 def auto_correct_gender(value, userData):
     print('USERDATA ', userData)
     predict_rs = gender_predictor.predict([(userData['height'], userData['weight'])])
@@ -16,7 +17,6 @@ correcter_mapping = {
     'gender': auto_correct_gender
 }
 
-@task
 def auto_correct(field: str, value, userData):
     print('RUN correcter with field: ', field)
     if field in correcter_mapping:
