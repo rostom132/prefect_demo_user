5c91132e2207d75b2add60060487b7826b8f9e29
Tien Tran <tien.trandinh99@gmai.com>
2024-05-19 14:25:34 +0700

    test

--- flow.py
+++ flow.py
@@ -3,6 +3,19 @@ from prefect import flow, task
 from model.user_modal_pre import UserModelPre
 from model.user_raw_model import UserRaw
 
+@task(retries=2, retry_delay_seconds=3)
+def validate_userData(userJson):
+    is_success = False
+    try:
+        userData = UserModelPre(**userJson)
+        is_success = True
+    except Exception as e:
+        print('FAILED to parse: ', userJson, e)
+        userData = UserRaw(**userJson)
+        is_success = False
+    
+    return is_success, userData
+
 @task(retries=2)
 def validate_input(input: str):
     print('PROCESS - validate user: ', input)
@@ -10,14 +23,14 @@ def validate_input(input: str):
     list_failed_data = []
 
     allUsersData = json.loads(input)
-    for userJson in allUsersData:
-        try:
-            userData = UserModelPre(**userJson)
+    list_rs = validate_userData.map(allUsersData)
+
+    for is_success, userData in list_rs:
+        if is_success:
             list_correct_data.append(userData)
-        except Exception as e:
-            print('FAILED to parse: ', userJson, e)
-            userData = UserRaw(**userJson)
+        else:
             list_failed_data.append(userData)
+
     return list_correct_data, list_failed_data
 
 @task(retries=3, retry_delay_seconds=3)
